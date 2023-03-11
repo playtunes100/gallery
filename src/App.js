e7b84210d2162de94e3a794217b8b32fd41fb526
Chikane Mashaba <playtunes100@gmail.com>
2023-03-11 23:13:19 +0200

    performance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import * as THREE from "three"
-import { Canvas, useLoader} from '@react-three/fiber'
-import { Sky, Image, Text3D, Box, OrbitControls} from '@react-three/drei'
+import { Suspense, useState } from 'react'
+import {Canvas, useLoader} from '@react-three/fiber'
+import {Sky, Image,PerformanceMonitor, Text3D, Box, OrbitControls} from '@react-three/drei'
 import font from './assets/fonts/Press.json'
 import Grass from './Grass.js'
 import pic from './assets/images/naked.png'
@@ -19,17 +20,20 @@ function Display(position, ...props){
 }
 
 function App() {
+  const [dpr, setDpr] = useState(1.5)
   return (
     <div className='App' style={{ width: window.innerWidth, height: window.innerHeight }}>
+      <Suspense fallback={<span>loading...</span>}>
       <Canvas >
-        
+      <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(1)} >
         <Sky azimuth={100} inclination={0.8} distance={400} mieCoefficient={0} />
         <Grass width={400} instances={1000000} />
-        <Display position={[-3,10,0]} scale={5}/>
-        <Text3D position={[0,5,0]} scale={5} font={font}>K G O T H A T S O</Text3D>
+        <Image url={pic} scale={5} position={[0,4,0]} />
+        <Text3D position={[0,5,0]} scale={5} font={font}>C H I K A N E</Text3D>
         <OrbitControls />
-        
+        </PerformanceMonitor>
       </Canvas>
+      </Suspense>
     </div>
   );
 }
