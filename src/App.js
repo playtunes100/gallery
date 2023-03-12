a3a5bb5881c57e382417fe5286b26d3531a42c01
Chikane Mashaba <playtunes100@gmail.com>
2023-03-12 09:15:56 +0200

    performance test

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function Display(position, ...props){
 
 
   return(
-    <group>
-      <Box scale={5} position={position}> <meshBasicMaterial transparent attach="material" map={useLoader(THREE.TextureLoader, pic)}/> </Box>
-     
+    <group props>
+      
+      <Image url={pic} scale={5} position={position} />
     </group>
     
   )
@@ -27,8 +27,8 @@ function App() {
       <Canvas dpr={dpr} camera={{ position: [0, 4, 4], fov: 75, near: 0.5, far: 100 }}>
       <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(0.5)} >
         <Sky azimuth={100} inclination={0.8} distance={400} mieCoefficient={0} />
-        <Grass width={400} instances={1000000} />
-        <Image url={pic} scale={5} position={[0,4,0]} />
+        <Grass width={4} instances={100} />
+        <Display position={[0,4,0]}/>
         <Text3D position={[-10,4,-5]} scale={1} font={font}>C H I K A N E</Text3D>
         <OrbitControls rotation={[30, 30, 0]} />
         </PerformanceMonitor>
