7db1497eeaaa093a2ef9ab31efb369b43b18c612
Chikane Mashaba <playtunes100@gmail.com>
2023-03-14 17:05:06 +0200

    small

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import * as THREE from "three"
-import { Suspense, useState } from 'react'
-import {Canvas, useLoader} from '@react-three/fiber'
-import {Sky, Image,PerformanceMonitor, Text3D, Box, OrbitControls} from '@react-three/drei'
+import { Suspense, useState,  } from 'react'
+import {Canvas} from '@react-three/fiber'
+import {Sky, Image,PerformanceMonitor, Text3D, OrbitControls} from '@react-three/drei'
 import font from './assets/fonts/Press.json'
 import Grass from './Grass.js'
 import pic from './assets/images/naked.png'
@@ -19,15 +19,17 @@ function Display(position, ...props){
   )
 }
 
-function App() {
+
+function App(...props) {
   const [dpr, setDpr] = useState(0.5)
   return (
     <div className='App' style={{ width: window.innerWidth, height: window.innerHeight }}>
       <Suspense fallback={<span>loading...</span>}>
       <Canvas dpr={dpr} camera={{ position: [0, 4, 4], fov: 75, near: 0.5, far: 100 }}>
+      
       <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(0.5)} >
         <Sky azimuth={100} inclination={0.8} distance={400} mieCoefficient={0} />
-        <Grass />
+        <Grass width={5} instances={500} />
         <Display position={[0,4,0]}/>
         <Text3D position={[-10,4,-5]} scale={1} font={font} > <meshBasicMaterial color={'#0000FF'} /> C H I K A N E</Text3D>
         <OrbitControls rotation={[30, 30, 0]} />
