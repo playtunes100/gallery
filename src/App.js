989cf4c04363aeb8697c7b62c328f152f3f4bd46
Chikane Mashaba <playtunes100@gmail.com>
2023-03-26 14:16:38 +0200

    huh

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import * as THREE from "three"
 import { Suspense, useState,  } from 'react'
-import {Canvas} from '@react-three/fiber'
-import {Sky, Image,PerformanceMonitor, Text3D, OrbitControls} from '@react-three/drei'
+import {Canvas, useLoader} from '@react-three/fiber'
+import {Sky, Stars,Box, Image,PerformanceMonitor, Text3D, FirstPersonControls, CameraControls, FlyControls} from '@react-three/drei'
 import font from './assets/fonts/Press.json'
 import Grass from './Grass.js'
 import pic from './assets/images/naked.png'
+import { Camera } from 'three';
 
-function Display(position, ...props){
-
+function Display(position, img, scale=5,...props){
+  const [me, setMe] = useState(pic)
 
   return(
-    <group props>
+    <group>
       
-      <Image url={pic} scale={5} position={position} />
+      <Box args={[scale, scale,scale]} scale={5} position={position} >
+        <meshBasicMaterial attach="material" map={useLoader(THREE.TextureLoader, pic)}/>
+      </Box>
+        
     </group>
     
   )
@@ -25,14 +29,16 @@ function App(...props) {
   return (
     <div className='App' style={{ width: window.innerWidth, height: window.innerHeight }}>
       <Suspense fallback={<span>loading...</span>}>
-      <Canvas dpr={dpr} camera={{ position: [0, 4, 4], fov: 75, near: 0.5, far: 100 }}>
+      <Canvas dpr={dpr} camera={{ position: [100, 5, 1e-5], rotation: [0,1.5708,0], fov: 75, near: 0.5, far: 1000 }}>
       
       <PerformanceMonitor onIncline={() => setDpr(2)} onDecline={() => setDpr(0.5)} >
-        <Sky azimuth={100} inclination={0.8} distance={400} mieCoefficient={0} />
+        <Sky azimuth={100} inclination={0.8} distance={200} mieCoefficient={0} />
+        <Stars radius={50} />
         <Grass />
-        <Display position={[0,4,0]}/>
+        <Image url={pic} scale={5} position={[8,4,0]} />
+        <Display img={pic} scale={5} position={[0,4,0]}/>
         <Text3D position={[-10,4,-5]} scale={1} font={font} > <meshBasicMaterial color={'#0000FF'} /> C H I K A N E</Text3D>
-        <OrbitControls rotation={[30, 30, 0]} />
+        <FlyControls/>
         </PerformanceMonitor>
       </Canvas>
       </Suspense>
